17419cca3c55a908799d40247d94fb8fc4d77ddf
Jihyun Kang <jihyunkang@lablup.com>
2025-11-13 16:03:05 +0900

    update: remove VLLM environment variables

--- models/llama-3-Korean-Bllossom-8B-awq/run-model-with-vllm.sh
+++ models/llama-3-Korean-Bllossom-8B-awq/run-model-with-vllm.sh
@@ -4,12 +4,6 @@
 cp .env.template .env
 source .env
 
-# add VLLM environment variable
-export VLLM_ATTENTION_BACKEND=TORCH_SDPA
-export VLLM_USE_FLASH_ATTENTION=0
-export VLLM_USE_FLASHINFER=0
-export CUDA_LAUNCH_BLOCKING=1
-
 # download model files from huggingface if not already present
 if [ ! -f "${MODEL_PATH}/config.json" ]; then
     pip install huggingface_hub
